75f25b8a19dfb1d84b996812ce2883b35877a899
markgray <mark.t.gray@gmail.com>
2020-11-10 06:13:29 -0500

    Added comments to BasicTransitionFragment in BasicTransition

--- BasicTransition/Application/src/main/java/com/example/android/basictransition/BasicTransitionFragment.kt
+++ BasicTransition/Application/src/main/java/com/example/android/basictransition/BasicTransitionFragment.kt
@@ -25,27 +25,81 @@ import android.view.ViewGroup
 import android.widget.RadioGroup
 import androidx.fragment.app.Fragment
 
+/**
+ * Our demo [Fragment]. The UI shows radioboxes to select between different Scenes,
+ * and uses various ways to transition between them.
+ */
 class BasicTransitionFragment : Fragment(), RadioGroup.OnCheckedChangeListener {
+
     // We transition between these Scenes
-    private var mScene1: Scene? = null
+    /**
+     * [Scene] instantiated from a live view hierarchy retrieved from view with ID R.id.container
+     */
+    private lateinit var mScene1: Scene
+
+    /**
+     * [Scene] inflated from the layout resource file [R.layout.scene2]
+     */
     private var mScene2: Scene? = null
-    private var mScene3: Scene? = null
+    /**
+     * [Scene] inflated from the layout resource file [R.layout.scene3]
+     */
+    private lateinit var mScene3: Scene
 
-    /** A custom TransitionManager  */
-    private var mTransitionManagerForScene3: TransitionManager? = null
+    /**
+     * A custom [TransitionManager]
+     */
+    private lateinit var mTransitionManagerForScene3: TransitionManager
 
-    /** Transitions take place in this ViewGroup. We retain this for the dynamic transition on scene 4.  */
-    private var mSceneRoot: ViewGroup? = null
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_basic_transition, container, false)!!
+    /**
+     * Transitions take place in this ViewGroup. We retain this for the dynamic transition on scene 4.
+     */
+    private lateinit var mSceneRoot: ViewGroup
+
+    /**
+     * Called to have the fragment instantiate its user interface view. First we initialize our [View]
+     * variable `val view` by using our [LayoutInflater] parameter [inflater] in flate our layout file
+     * [R.layout.fragment_basic_transition], using our [ViewGroup] parameter [container] for its
+     * LayoutParams without attaching to it. We initialize our [RadioGroup] variable `val radioGroup`
+     * by finding the [View] in `view` with ID [R.id.select_scene] and set its `OnCheckedChangeListener`
+     * to `this`. We initialize our [ViewGroup] field [mSceneRoot] by finding the [View] in `view` with
+     * ID [R.id.select_scene]. We then initialize our [Scene] field [mScene1] with an instance which
+     * when entered, will remove any children from the [mSceneRoot] container and add the view in
+     * [mSceneRoot] with ID [R.id.container] as a new child of [mSceneRoot]. We initialize our [Scene]
+     * field [mScene2] with an instance described by the resource file [R.layout.scene2] with
+     * [mSceneRoot] as root of the hierarchy in which scene changes and transitions will take place,
+     * and our [Scene] field [mScene3] with an instance described by the resource file [R.layout.scene3]
+     * with [mSceneRoot] as root of the hierarchy in which scene changes and transitions will take
+     * place. We initialize our [TransitionManager] field [mTransitionManagerForScene3] with a custom
+     * TransitionManager for Scene 3 inflated from the file [R.transition.scene3_transition_manager]
+     * in which ChangeBounds and Fade take place at the same time. Finally we return `view` to our
+     * caller.
+     *
+     * @param inflater The [LayoutInflater] object that can be used to inflate any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's UI will be attached to.
+     * The fragment should not add the view itself, but this can be used to generate the LayoutParams
+     * of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     * @return Return the [View] for the fragment's UI, or null.
+     */
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(
+                R.layout.fragment_basic_transition,
+                container,
+                false
+        )!!
         val radioGroup = view.findViewById<View>(R.id.select_scene) as RadioGroup
         radioGroup.setOnCheckedChangeListener(this)
         mSceneRoot = view.findViewById<View>(R.id.scene_root) as ViewGroup
 
         // BEGIN_INCLUDE(instantiation_from_view)
         // A Scene can be instantiated from a live view hierarchy.
-        mScene1 = Scene(mSceneRoot, mSceneRoot!!.findViewById<View>(R.id.container))
+        mScene1 = Scene(mSceneRoot, mSceneRoot.findViewById<View>(R.id.container))
         // END_INCLUDE(instantiation_from_view)
 
         // BEGIN_INCLUDE(instantiation_from_resource)
@@ -65,6 +119,24 @@ class BasicTransitionFragment : Fragment(), RadioGroup.OnCheckedChangeListener {
         return view
     }
 
+    /**
+     * Called when the checked radio button has changed. When the selection is cleared, [checkedId]
+     * is -1. We switch on the value of our parameter [checkedId]:
+     *  - [R.id.select_scene_1] we use the [TransitionManager.go] method to start an automatic
+     *  transition to [Scene] field [mScene1] using the default transition for TransitionManager.
+     *  - [R.id.select_scene_2] we use the [TransitionManager.go] method to start an automatic
+     *  transition to [Scene] field [mScene2] using the default transition for TransitionManager.
+     *  - [R.id.select_scene_3] we use our custom [TransitionManager] field [mTransitionManagerForScene3]
+     *  to change to [Scene] field [mScene3], using the appropriate transition for this particular
+     *  scene change.
+     *  - [R.id.select_scene_4] we dynamically invoke a transition without a [Scene]. To do this we
+     *  first call the [TransitionManager.beginDelayedTransition] using [mSceneRoot] as the root of
+     *  the View hierarchy to run the transition on. and then we just change view properties of the
+     *  view with ID [R.id.transition_square] to change its size to 100dp by 100dp.
+     *
+     * @param group the [RadioGroup] in which the checked radio button has changed
+     * @param checkedId the unique identifier of the newly checked radio button
+     */
     override fun onCheckedChanged(group: RadioGroup, checkedId: Int) {
         when (checkedId) {
             R.id.select_scene_1 -> {
@@ -80,7 +152,7 @@ class BasicTransitionFragment : Fragment(), RadioGroup.OnCheckedChangeListener {
 
                 // BEGIN_INCLUDE(transition_custom)
                 // You can also start a transition with a custom TransitionManager.
-                mTransitionManagerForScene3!!.transitionTo(mScene3)
+                mTransitionManagerForScene3.transitionTo(mScene3)
             }
             R.id.select_scene_4 -> {
 
@@ -89,7 +161,7 @@ class BasicTransitionFragment : Fragment(), RadioGroup.OnCheckedChangeListener {
                 // For this, we first call TransitionManager.beginDelayedTransition().
                 TransitionManager.beginDelayedTransition(mSceneRoot)
                 // Then, we can just change view properties as usual.
-                val square = mSceneRoot!!.findViewById<View>(R.id.transition_square)
+                val square = mSceneRoot.findViewById<View>(R.id.transition_square)
                 val params = square.layoutParams
                 val newSize = resources.getDimensionPixelSize(R.dimen.square_size_expanded)
                 params.width = newSize
