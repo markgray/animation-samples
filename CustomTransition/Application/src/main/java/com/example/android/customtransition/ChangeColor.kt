6dd9e476c6c9f01c7d0eabd63af15b2dee2284dd
markgray <mark.t.gray@gmail.com>
2020-11-12 06:12:30 -0500

    Converted ChangeColor.java in CustomTransition to kotlin

--- CustomTransition/Application/src/main/java/com/example/android/customtransition/ChangeColor.kt
+++ CustomTransition/Application/src/main/java/com/example/android/customtransition/ChangeColor.kt
@@ -13,107 +13,95 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.customtransition
 
-package com.example.android.customtransition;
+import android.animation.Animator
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.transition.Transition
+import android.transition.TransitionValues
+import android.view.ViewGroup
 
-import android.animation.Animator;
-import android.animation.ArgbEvaluator;
-import android.animation.ObjectAnimator;
-import android.animation.PropertyValuesHolder;
-import android.animation.ValueAnimator;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.TransitionDrawable;
-import android.transition.ChangeBounds;
-import android.transition.Transition;
-import android.transition.TransitionValues;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewPropertyAnimator;
-
-public class ChangeColor extends Transition {
-
-    /** Key to store a color value in TransitionValues object */
-    private static final String PROPNAME_BACKGROUND = "customtransition:change_color:background";
-
+class ChangeColor : Transition() {
     // BEGIN_INCLUDE (capture_values)
     /**
      * Convenience method: Add the background Drawable property value
      * to the TransitionsValues.value Map for a target.
      */
-    private void captureValues(TransitionValues values) {
+    private fun captureValues(values: TransitionValues) {
         // Capture the property values of views for later use
-        values.values.put(PROPNAME_BACKGROUND, values.view.getBackground());
+        values.values[PROPNAME_BACKGROUND] = values.view.background
     }
 
-    @Override
-    public void captureStartValues(TransitionValues transitionValues) {
-        captureValues(transitionValues);
+    override fun captureStartValues(transitionValues: TransitionValues) {
+        captureValues(transitionValues)
     }
 
     // Capture the value of the background drawable property for a target in the ending Scene.
-    @Override
-    public void captureEndValues(TransitionValues transitionValues) {
-        captureValues(transitionValues);
+    override fun captureEndValues(transitionValues: TransitionValues) {
+        captureValues(transitionValues)
     }
-    // END_INCLUDE (capture_values)
 
+    // END_INCLUDE (capture_values)
     // BEGIN_INCLUDE (create_animator)
     // Create an animation for each target that is in both the starting and ending Scene. For each
     // pair of targets, if their background property value is a color (rather than a graphic),
     // create a ValueAnimator based on an ArgbEvaluator that interpolates between the starting and
     // ending color. Also create an update listener that sets the View background color for each
     // animation frame
-    @Override
-    public Animator createAnimator(ViewGroup sceneRoot,
-                                   TransitionValues startValues, TransitionValues endValues) {
+    override fun createAnimator(sceneRoot: ViewGroup,
+                                startValues: TransitionValues?,
+                                endValues: TransitionValues?
+    ): Animator? {
         // This transition can only be applied to views that are on both starting and ending scenes.
         if (null == startValues || null == endValues) {
-            return null;
+            return null
         }
         // Store a convenient reference to the target. Both the starting and ending layout have the
         // same target.
-        final View view = endValues.view;
+        val view = endValues.view
         // Store the object containing the background property for both the starting and ending
         // layouts.
-        Drawable startBackground = (Drawable) startValues.values.get(PROPNAME_BACKGROUND);
-        Drawable endBackground = (Drawable) endValues.values.get(PROPNAME_BACKGROUND);
+        val startBackground = startValues.values[PROPNAME_BACKGROUND] as Drawable?
+        val endBackground = endValues.values[PROPNAME_BACKGROUND] as Drawable?
         // This transition changes background colors for a target. It doesn't animate any other
         // background changes. If the property isn't a ColorDrawable, ignore the target.
-        if (startBackground instanceof ColorDrawable && endBackground instanceof ColorDrawable) {
-            ColorDrawable startColor = (ColorDrawable) startBackground;
-            ColorDrawable endColor = (ColorDrawable) endBackground;
+        if (startBackground is ColorDrawable && endBackground is ColorDrawable) {
             // If the background color for the target in the starting and ending layouts is
             // different, create an animation.
-            if (startColor.getColor() != endColor.getColor()) {
+            if (startBackground.color != endBackground.color) {
                 // Create a new Animator object to apply to the targets as the transitions framework
                 // changes from the starting to the ending layout. Use the class ValueAnimator,
                 // which provides a timing pulse to change property values provided to it. The
                 // animation runs on the UI thread. The Evaluator controls what type of
                 // interpolation is done. In this case, an ArgbEvaluator interpolates between two
                 // #argb values, which are specified as the 2nd and 3rd input arguments.
-                ValueAnimator animator = ValueAnimator.ofObject(new ArgbEvaluator(),
-                        startColor.getColor(), endColor.getColor());
+                val animator = ValueAnimator.ofObject(ArgbEvaluator(),
+                        startBackground.color, endBackground.color)
                 // Add an update listener to the Animator object.
-                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                    @Override
-                    public void onAnimationUpdate(ValueAnimator animation) {
-                        Object value = animation.getAnimatedValue();
-                        // Each time the ValueAnimator produces a new frame in the animation, change
-                        // the background color of the target. Ensure that the value isn't null.
-                        if (null != value) {
-                            view.setBackgroundColor((Integer) value);
-                        }
+                animator.addUpdateListener { animation ->
+                    val value = animation.animatedValue
+                    // Each time the ValueAnimator produces a new frame in the animation, change
+                    // the background color of the target. Ensure that the value isn't null.
+                    if (null != value) {
+                        view.setBackgroundColor((value as Int))
                     }
-                });
+                }
                 // Return the Animator object to the transitions framework. As the framework changes
                 // between the starting and ending layouts, it applies the animation you've created.
-                return animator;
+                return animator
             }
         }
         // For non-ColorDrawable backgrounds, we just return null, and no animation will take place.
-        return null;
-    }
-    // END_INCLUDE (create_animator)
+        return null
+    } // END_INCLUDE (create_animator)
 
+    companion object {
+        /**
+         * Key to store a color value in TransitionValues object
+         */
+        private const val PROPNAME_BACKGROUND = "customtransition:change_color:background"
+    }
 }
