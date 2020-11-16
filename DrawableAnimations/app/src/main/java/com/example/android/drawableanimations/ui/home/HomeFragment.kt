821bbf5effaec8fdbdaa9b54067aa534984af7ac
markgray <mark.t.gray@gmail.com>
2020-11-16 06:25:41 -0500

    Added comments to DrawableAnimations

--- DrawableAnimations/app/src/main/java/com/example/android/drawableanimations/ui/home/HomeFragment.kt
+++ DrawableAnimations/app/src/main/java/com/example/android/drawableanimations/ui/home/HomeFragment.kt
@@ -36,8 +36,38 @@ import com.example.android.drawableanimations.viewBindings
  */
 class HomeFragment : Fragment(R.layout.home_fragment) {
 
+    /**
+     * The [HomeFragmentBinding] view binding created from our layout file layout/home_fragment.xml
+     * `viewBindings` is an extension function of [Fragment] created in `ViewBindingDelegates.kt`
+     */
     private val binding by viewBindings(HomeFragmentBinding::bind)
 
+    /**
+     * Called immediately after [onCreateView] has returned, but before any saved state has been
+     * restored in to the view. This gives subclasses a chance to initialize themselves once they
+     * know their view hierarchy has been completely created. The fragment's view hierarchy is not
+     * however attached to its parent at this point.
+     *
+     * We initialize our [DemoListAdapter] variable `val adapter` with a new instance constructed
+     * to use a lambda for the `demoClicked` field that is used as the `OnClickListener` for the
+     * item view held by the `DemoViewHolder` class. When the item is clicked the lambda uses the
+     * `FragmentActivity` this fragment is currently associated with to fetch the `FragmentManager`
+     * for interacting with fragments associated with the activity to commit a `FragmentTransaction`
+     * with the [Fragment] operations to replace the fragment in the container with ID [R.id.main]
+     * with the [Fragment] that the `createFragment` field of the [Demo] object held by the
+     * `DemoViewHolder` that was clicked (either [AnimatedFragment], or [SeekableFragment] in our
+     * case), and adds the `FragmentTransaction` to the back stack. Then the lambda sets the title
+     * of the `FragmentActivity` to the `title` field of the [Demo] that was clicked.
+     *
+     * Having constructed our [DemoListAdapter] we set the adapter of the `RecyclerView` in our
+     * [HomeFragmentBinding] field [binding] whose ID is `list` to this `adapter`. We then submit
+     * a list of [Demo] objects to `adapter` for our fragments [AnimatedFragment] (titled
+     * "AnimatedVectorDrawableCompat") and [SeekableFragment] (titled "SeekableAnimatedVectorDrawable")
+     *
+     * @param view The [View] returned by [onCreateView].
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val adapter = DemoListAdapter { demo ->
             activity?.let { activity ->
@@ -55,6 +85,14 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
         ))
     }
 
+    /**
+     * Called when the fragment's activity has been created and this fragment's view hierarchy
+     * instantiated. We call our super's implementation of `onActivityCreated` then set the
+     * title of our `FragmentActivity` to "DrawableAnimations".
+     *
+     * @param savedInstanceState If the fragment is being re-created from
+     * a previous saved state, this is the state.
+     */
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         activity?.setTitle(R.string.app_name)
