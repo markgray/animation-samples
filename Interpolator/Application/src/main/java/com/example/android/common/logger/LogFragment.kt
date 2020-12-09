8ae41970f89f65bcc7074af856f1e6461cc7a7ce
markgray <mark.t.gray@gmail.com>
2020-12-09 06:05:40 -0500

    Added comments to Interpolator

--- Interpolator/Application/src/main/java/com/example/android/common/logger/LogFragment.kt
+++ Interpolator/Application/src/main/java/com/example/android/common/logger/LogFragment.kt
@@ -42,16 +42,40 @@ import android.widget.ScrollView
 import androidx.fragment.app.Fragment
 
 /**
- * Simple fraggment which contains a LogView and uses is to output log data it receives
- * through the LogNode interface.
+ * Simple fragment which contains a [LogView] and uses it to output log data it receives
+ * through the [LogNode] interface.
  */
 class LogFragment : Fragment() {
+    /**
+     * The [LogView] in our UI which is used to display log data, it is a custom `AppCompatTextView`
+     * which implements the [LogNode] interface.
+     */
     lateinit var logView: LogView
         private set
+
+    /**
+     * The [ScrollView] in our UI which holds our [LogView] field [logView]
+     */
     private lateinit var mScrollView: ScrollView
 
     /**
+     * Builds our UI layout using code, and returns the resulting [View] to the caller. First we
+     * initialize our [ScrollView] field [mScrollView] with a new instance, create an instance of
+     * [ViewGroup.LayoutParams] whose width and height are both `MATCH_PARENT` to initialize our
+     * variable `val scrollParams`, and set the layout params of [mScrollView] to it. We initialize
+     * our [LogView] field [logView] with a new instance, initialize our variable `val logParams` to
+     * a copy of `scrollParams`, set its height to `WRAP_CONTENT`, and then set the layout params of
+     * [logView] to `logParams`. We enable click events for [logView], enable it to receive focus,
+     * and set its typeface to [Typeface.MONOSPACE]. We use the logical density of the display to
+     * calculate how many pixels there are in 16 dips to initialize our variable `val paddingPixels`
+     * then set the padding on all sides of [logView] to `paddingPixels`, and also set the size of
+     * the padding between the compound drawables and the text to `paddingPixels`. We set the
+     * `gravity` of [logView] to [Gravity.BOTTOM] and set the text appearance to the system style
+     * resource `TextAppearance_Holo_Medium` (using the one argument version of `setTextAppearance`
+     * for devices `M` and newer, and the deprecated two argument version for older devices). We
+     * then add [logView] to [mScrollView] and return [mScrollView] to the caller.
      *
+     * @return a [View] which consists of a [ScrollView] holding our [LogView] field [logView].
      */
     private fun inflateViews(): View {
         mScrollView = ScrollView(activity)
@@ -85,9 +109,30 @@ class LogFragment : Fragment() {
         return mScrollView
     }
 
+    /**
+     * Called to have the fragment instantiate its user interface view. This will be called between
+     * [onCreate] and [onActivityCreated]. It is recommended to **only** inflate the layout in this
+     * method and move logic that operates on the returned [View] to [onViewCreated]. We initialize
+     * our [View] variable `val result` to the [View] returned by our [inflateViews] method, and then
+     * add a [TextWatcher] to [logView] whose `afterTextChanged` override scrolls [mScrollView] to
+     * the bottom of the view whenever the text in [logView] changes. Finally we return `result` to
+     * the caller.
+     *
+     * @param inflater The [LayoutInflater] object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI will be attached to. The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     * @return Return the [View] for the fragment's UI, or `null`.
+     */
     @Suppress("RedundantNullableReturnType")
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val result = inflateViews()
         logView.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
