f02aeabafbd1d9837038da99f95f6dc709ea0f54
markgray <mark.t.gray@gmail.com>
2020-12-24 06:50:05 -0500

    Added comments to Motion

--- Motion/app/src/main/java/com/example/android/motion/demo/dissolve/DissolveActivity.kt
+++ Motion/app/src/main/java/com/example/android/motion/demo/dissolve/DissolveActivity.kt
@@ -16,13 +16,17 @@
 
 package com.example.android.motion.demo.dissolve
 
+import android.animation.TimeInterpolator
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.transition.TransitionManager
 import com.example.android.motion.R
 import com.example.android.motion.demo.FAST_OUT_SLOW_IN
@@ -57,6 +61,35 @@ class DissolveActivity : AppCompatActivity() {
      * `MaterialCardView` with the [ImageView] with ID [R.id.card] with a [MaterialButton] with ID
      * [R.id.next] below it.
      *
+     * We initialize our [Toolbar] variable `val toolbar` by finding the view with ID [R.id.toolbar],
+     * our [ImageView] variable `val image` by finding the view with ID [R.id.image], our
+     * [MaterialCardView] variable `val card` by finding the view with ID [R.id.card], and our
+     * [MaterialButton] variable `val next` by finding the view with ID [R.id.next]. We call the
+     * method [setSupportActionBar] to set `toolbar` to act as the `ActionBar` for our activities
+     * window. We call our [EdgeToEdge.setUpRoot] method to set the `systemUiVisibility` of the
+     * [ViewGroup] with ID [R.id.root] (the "root' `CoordinatorLayout` of our layout file) to
+     * [View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION] (requests that the system navigation be hidden)
+     * and [View.SYSTEM_UI_FLAG_LAYOUT_STABLE] (we would like a stable view of the content insets
+     * given, ie. the insets seen there will always represent the worst case that the application
+     * can expect as a continuous state). We call the [EdgeToEdge.setUpAppBar] with the view with
+     * ID [R.id.app_bar] (the `AppBarLayout` in our layout file) and `toolbar` to have it apply
+     * padding to the `AppBarLayout` and [Toolbar] variable `toolbar`. We call our method
+     * [EdgeToEdge.setUpScrollingContent] with the view with ID [R.id.content] (the `ConstraintLayout`
+     * in our layout file) to have it apply padding to the [ViewGroup].
+     *
+     * Next we initialize our [Dissolve] variable `val dissolve` to an instance configured using the
+     * `apply` extension function to use `image` as the target view, with a duration of 200 ms, and
+     * our [TimeInterpolator] of [FAST_OUT_SLOW_IN] (defined in our file demo/Interpolators.kt).
+     *
+     * We then set an [Observer] on the [LiveData] wrapped resource ID property `image` of our
+     * [DissolveViewModel] field [viewModel] whose lambda begins the transition `dissolve` on our
+     * [ViewGroup] variable `card`, then sets the drawable with the resource ID of that `image`
+     * property to be the content of this [ImageView] variable `image` (the animation to that
+     * drawable is then handled by the transition API).
+     *
+     * Finally we set the `OnClickListener` of both `card` and `next` to lambdas which call the
+     * `nextImage` method of [viewModel].
+     *
      * @param savedInstanceState If the activity is being re-initialized after previously being shut
      * down then this [Bundle] contains the data it most recently supplied in [onSaveInstanceState].
      * __Note: Otherwise it is null.__ We do not override [onSaveInstanceState] so do not use it.
@@ -76,7 +109,7 @@ class DissolveActivity : AppCompatActivity() {
         EdgeToEdge.setUpScrollingContent(findViewById(R.id.content))
 
         // This is the transition we use for dissolve effect of the image view.
-        val dissolve = Dissolve().apply {
+        val dissolve: Dissolve = Dissolve().apply {
             addTarget(image)
             duration = 200L
             interpolator = FAST_OUT_SLOW_IN
