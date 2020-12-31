97a99ebec411b1ec1e1378529206a20eb6cd7bda
markgray <mark.t.gray@gmail.com>
2020-12-31 06:38:21 -0500

    Added comments to Motion

--- Motion/app/src/main/java/com/example/android/motion/demo/fadethrough/FadeThroughActivity.kt
+++ Motion/app/src/main/java/com/example/android/motion/demo/fadethrough/FadeThroughActivity.kt
@@ -22,7 +22,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
+import androidx.transition.Fade
+import androidx.transition.Transition
 import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.example.android.motion.R
@@ -41,6 +44,37 @@ import com.google.android.material.card.MaterialCardView
  */
 class FadeThroughActivity : AppCompatActivity() {
 
+    /**
+     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
+     * then we set our content view to our layout file [R.layout.fade_through_activity]. Next we
+     * locate the various widgets in our UI:
+     *  - [Toolbar] variable `val toolbar` with ID [R.id.toolbar]
+     *  - [MaterialCardView] variable `val card` with ID [R.id.card]
+     *  - [ConstraintLayout] variable `val contact` with ID [R.id.card_contact]
+     *  - [ConstraintLayout] variable `val cheese` with ID [R.id.card_cheese]
+     *  - [MaterialButton] variable `val toggle` with ID [R.id.toggle]
+     *  - [ImageView] variable `val icon` with ID [R.id.contact_icon]
+     *
+     * We then call the [setSupportActionBar] method to set `toolbar` to act as the ActionBar for
+     * this Activity's window, and call the various [EdgeToEdge] methods to set up our UI for
+     * edge to edge display. We begin a load with [Glide] that will load our [R.drawable.cheese_2]
+     * drawable into `icon` after applying a circle crop transformation.
+     *
+     * We initialize our [Transition] variable `val fadeThrough` with a new instance of our
+     * [fadeThrough] transition (it is a [TransitionSet] which performs a [Fade.OUT] followed by
+     * a [Fade.IN]).
+     *
+     * Finally we set the `OnClickListener` of our `toggle` button to a lambda which branches on
+     * whether our `contact` [ConstraintLayout] is visible or not:
+     *  - `isVisible` is `true`: we use the [TransitionManager] to begin begin a delayed animation of
+     *  `card` using our `fadeThrough` [Transition] when the toggling of the visibility of `contact`
+     *  to invisible and the toggling of the visibility of `cheese` to visible takes effect.
+     *  - `isVisible` is `false`: we use the [TransitionManager] to begin begin a delayed animation of
+     *  `card` using our `fadeThrough` [Transition] when the toggling of the visibility of `contact`
+     *  to visible and the toggling of the visibility of `cheese` to invisible takes effect.
+     *
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fade_through_activity)
@@ -60,12 +94,13 @@ class FadeThroughActivity : AppCompatActivity() {
         Glide.with(icon).load(R.drawable.cheese_2).transform(CircleCrop()).into(icon)
 
         // This is the transition we use for the fade-through effect.
-        val fadeThrough = fadeThrough()
+        val fadeThrough: Transition = fadeThrough()
 
         toggle.setOnClickListener {
             // We are only toggling the visibilities of the card contents here.
             if (contact.isVisible) {
-                // Delays the fade-through transition until the layout change below takes effect.
+                // Delays the fade-through transition until the layout change caused by changing
+                // the visibility of `contact` and `cheese` below takes effect.
                 TransitionManager.beginDelayedTransition(
                     card,
                     fadeThrough.setDuration(MEDIUM_EXPAND_DURATION)
@@ -73,6 +108,8 @@ class FadeThroughActivity : AppCompatActivity() {
                 contact.isVisible = false
                 cheese.isVisible = true
             } else {
+                // Delays the fade-through transition until the layout change caused by changing
+                // the visibility of `contact` and `cheese` below takes effect.
                 TransitionManager.beginDelayedTransition(
                     card,
                     fadeThrough.setDuration(MEDIUM_COLLAPSE_DURATION)
