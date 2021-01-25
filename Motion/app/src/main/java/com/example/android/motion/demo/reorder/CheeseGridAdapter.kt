033f82dcf72bcab740b6f65f93f5e7e0667cfe67
markgray <mark.t.gray@gmail.com>
2021-01-25 06:10:47 -0500

    Added comments to Motion

--- Motion/app/src/main/java/com/example/android/motion/demo/reorder/CheeseGridAdapter.kt
+++ Motion/app/src/main/java/com/example/android/motion/demo/reorder/CheeseGridAdapter.kt
@@ -40,13 +40,44 @@ class CheeseGridAdapter(
 ) : ListAdapter<Cheese, CheeseViewHolder>(Cheese.DIFF_CALLBACK) {
 
     init {
+        /**
+         * Indicate that each item in the data set can be represented with a unique identifier
+         * of type `Long`. We call with `true` because our `getItemId` override returns the
+         * `id` property of the `Cheese` in the position and that property is set to the position
+         * of its `Cheese.name` property in the `Cheese.NAMES` list plus 1 guaranteeing a unique ID.
+         */
         setHasStableIds(true)
     }
 
+    /**
+     * Return the stable ID for the item at [position]. If [hasStableIds] would return `false` this
+     * method should return `NO_ID``. The default implementation of this method returns `NO_ID`.
+     * The [Cheese.id] property of each [Cheese] in our dataset is stable and unique as it is set to
+     * the index of its [Cheese.name] property in the [Cheese.NAMES] list plus 1. So we just return
+     * the [Cheese.id] property of the [Cheese] object at position [position] in the current list
+     * being used as our dataset by our [ListAdapter] super.
+     *
+     * @param position Adapter position to query
+     * @return the stable ID of the item at position
+     */
     override fun getItemId(position: Int): Long {
         return getItem(position).id
     }
 
+    /**
+     * Called when [RecyclerView] needs a new [CheeseViewHolder] of the given type to represent an
+     * item. We return a new instance of [CheeseViewHolder] constructed to use our [ViewGroup]
+     * parameter [parent] for its layout params after setting the `OnLongClickListener` of its
+     * `itemView` to a lambda which calls our [onItemLongClick] field with a reference to the
+     * [CheeseViewHolder] then returns `true` to consume the event, and setting the `OnClickListener`
+     * of its `itemView` to a lambda which toasts the [Cheese.name] property of the [Cheese] held by
+     * the [CheeseViewHolder].
+     *
+     * @param parent The [ViewGroup] into which the new `View` will be added after it is bound to
+     * an adapter position.
+     * @param viewType The view type of the new `View`.
+     * @return A new ViewHolder that holds a `View` of the given view type.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CheeseViewHolder {
         return CheeseViewHolder(parent).apply {
             itemView.setOnLongClickListener {
@@ -65,11 +96,30 @@ class CheeseGridAdapter(
         }
     }
 
+    /**
+     * Called by [RecyclerView] to display the data at the specified position. This method should
+     * update the contents of the [CheeseViewHolder.itemView] to reflect the item at the given
+     * position. We just call the [CheeseViewHolder.bind] method of our [CheeseViewHolder] parameter
+     * [holder] with the [Cheese] at position [position] in our dataset.
+     *
+     * @param holder The [CheeseViewHolder] which should be updated to represent the contents of the
+     * item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     override fun onBindViewHolder(holder: CheeseViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 }
 
+/**
+ * The [RecyclerView.ViewHolder] that is used to display [Cheese] objects from our dataset in our
+ * [RecyclerView]. Our constructor just calls our super's constructor with the `View` that the
+ * [LayoutInflater] of the context of our [ViewGroup] parameter `parent` inflates from our layout
+ * file [R.layout.cheese_staggered_grid_item] when it uses `parent` for the layout params without
+ * attaching to it (that `View` becomes our [CheeseViewHolder.itemView]).
+ *
+ * @param parent the [ViewGroup] that we will be attached to (our [RecyclerView] in our case).
+ */
 class CheeseViewHolder(
     parent: ViewGroup
 ) : RecyclerView.ViewHolder(
