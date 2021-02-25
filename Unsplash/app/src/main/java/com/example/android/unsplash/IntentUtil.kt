846c773983ad330ac204606c03aadecf972bf968
markgray <mark.t.gray@gmail.com>
2021-02-25 07:56:22 -0500

    Converted to IntentUtil to kotlin and added comments in Unsplash

--- Unsplash/app/src/main/java/com/example/android/unsplash/IntentUtil.kt
+++ Unsplash/app/src/main/java/com/example/android/unsplash/IntentUtil.kt
@@ -13,37 +13,87 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.unsplash
 
-package com.example.android.unsplash;
-
-import android.content.Intent;
+import android.content.Intent
+import android.graphics.Rect
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.example.android.unsplash.data.model.Photo
+import java.util.ArrayList
 
 /**
  * Holding intent extra names and utility methods for intent handling.
  */
-public class IntentUtil {
-    public static final String FONT_SIZE = "fontSize";
-    public static final String PADDING = "padding";
-    public static final String PHOTO = "photo";
-    public static final String TEXT_COLOR = "color";
-    public static final String RELEVANT_PHOTOS = "relevant";
-    public static final String SELECTED_ITEM_POSITION = "selected";
-    public static final int REQUEST_CODE = R.id.requestCode;
+object IntentUtil {
+    /**
+     * The key for the extra in which [MainActivity] stores the `textSize` of the `TextView` holding
+     * the `author` property of the [Photo] that the user selected in the [Intent] it uses to launch
+     * [DetailActivity].
+     */
+    const val FONT_SIZE = "fontSize"
+
+    /**
+     * The key for the extra in which [MainActivity] stores a [Rect] holding the padding of the
+     * `TextView` holding the `author` property of the [Photo] that the user selected in the
+     * [Intent] it uses to launch [DetailActivity].
+     */
+    const val PADDING = "padding"
+
+    /**
+     * The key for the extra in which [MainActivity] stores a `ParcelableArrayList` holding all of
+     * the [Photo] objects in the [ArrayList] of [Photo] objects used as the shared dataset in the
+     * [Intent] it uses to launch [DetailActivity].
+     */
+    const val PHOTO = "photo"
+
+    /**
+     *  The key for the extra in which [MainActivity] stores the current color selected for normal
+     *  text of the `TextView` holding the `author` property of the [Photo] that the user selected
+     *  in the [Intent] it uses to launch [DetailActivity].
+     */
+    const val TEXT_COLOR = "color"
+
+    /**
+     * The key in the [Bundle] passed to the `onSaveInstanceState` override of [MainActivity] which
+     * is used to save the [ArrayList] of [Photo] objects used as its dataset (aka the field
+     * [MainActivity.relevantPhotos]) and the key which its `onCreate` override uses to restore
+     * [MainActivity.relevantPhotos] from the [Bundle] passed it when the activity is being
+     * re-initialized after previously being shut down.
+     */
+    const val RELEVANT_PHOTOS = "relevant"
+
+    /**
+     * The key for the [Intent] extra in which both [MainActivity] and [DetailActivity] store the
+     * position of the [Photo] object in their UI that was selected by the user. [MainActivity]
+     * stores the positon of the selected [Photo] in the [Intent] used to launch [DetailActivity],
+     * and in [DetailActivity] if the user scrolls to a different [Photo] in its [ViewPager] it will
+     * store the new position in an [Intent] which it returns to [MainActivity] as part of its
+     * result (if the user does not move to a new [Photo] no [Intent] is included).
+     */
+    const val SELECTED_ITEM_POSITION = "selected"
+
+    /**
+     * The `requestCode` that [MainActivity] uses to launch [DetailActivity] with in its call to the
+     * method `startActivityForResult`. Just an arbitrary [Int] which is ignored by both activities.
+     */
+    const val REQUEST_CODE = R.id.requestCode
 
     /**
      * Checks if all extras are present in an intent.
      *
      * @param intent The intent to check.
      * @param extras The extras to check for.
-     * @return <code>true</code> if all extras are present, else <code>false</code>.
+     * @return `true` if all extras are present, else `false`.
      */
-    public static boolean hasAll(Intent intent, String... extras) {
-        for (String extra : extras) {
+    @JvmStatic
+    fun hasAll(intent: Intent, vararg extras: String?): Boolean {
+        for (extra in extras) {
             if (!intent.hasExtra(extra)) {
-                return false;
+                return false
             }
         }
-        return true;
+        return true
     }
 
     /**
@@ -51,14 +101,15 @@ public class IntentUtil {
      *
      * @param intent The intent to check.
      * @param extras The extras to check for.
-     * @return <code>true</code> if any checked extra is present, else <code>false</code>.
+     * @return `true` if any checked extra is present, else `false`.
      */
-    public static boolean hasAny(Intent intent, String... extras) {
-        for (String extra : extras) {
+    @Suppress("unused")
+    fun hasAny(intent: Intent, vararg extras: String?): Boolean {
+        for (extra in extras) {
             if (intent.hasExtra(extra)) {
-                return true;
+                return true
             }
         }
-        return false;
+        return false
     }
 }
