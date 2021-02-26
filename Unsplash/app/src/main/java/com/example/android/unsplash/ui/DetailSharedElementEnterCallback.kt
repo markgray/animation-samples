66cdbaba01cbea615798ca3281adaa725c306ccf
markgray <mark.t.gray@gmail.com>
2021-02-26 07:18:22 -0500

    Converted DetailSharedElementEnterCallback to kotlin in Unsplash

--- Unsplash/app/src/main/java/com/example/android/unsplash/ui/DetailSharedElementEnterCallback.kt
+++ Unsplash/app/src/main/java/com/example/android/unsplash/ui/DetailSharedElementEnterCallback.kt
@@ -13,116 +13,125 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.unsplash.ui;
+package com.example.android.unsplash.ui
 
-import androidx.core.app.SharedElementCallback;
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import android.graphics.Rect;
-import android.util.TypedValue;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.Rect
+import android.util.TypedValue
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.app.SharedElementCallback
+import com.example.android.unsplash.IntentUtil
+import com.example.android.unsplash.IntentUtil.hasAll
+import com.example.android.unsplash.databinding.DetailViewBinding
+import com.example.android.unsplash.databinding.PhotoItemBinding
+import java.util.ArrayList
 
-import androidx.annotation.NonNull;
+class DetailSharedElementEnterCallback(
+    private val intent: Intent
+    ) : SharedElementCallback() {
 
-import com.example.android.unsplash.IntentUtil;
-import com.example.android.unsplash.databinding.DetailViewBinding;
-import com.example.android.unsplash.databinding.PhotoItemBinding;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-public class DetailSharedElementEnterCallback extends SharedElementCallback {
-
-    private final Intent intent;
-    private float targetTextSize;
-    private ColorStateList targetTextColors;
-    private DetailViewBinding currentDetailBinding;
-    private PhotoItemBinding currentPhotoBinding;
-    private Rect targetPadding;
-
-    public DetailSharedElementEnterCallback(Intent intent) {
-        this.intent = intent;
-    }
-
-    @Override
-    public void onSharedElementStart(List<String> sharedElementNames,
-                                     List<View> sharedElements,
-                                     List<View> sharedElementSnapshots) {
-        TextView author = getAuthor();
-        targetTextSize = author.getTextSize();
-        targetTextColors = author.getTextColors();
-        targetPadding = new Rect(author.getPaddingLeft(),
-                author.getPaddingTop(),
-                author.getPaddingRight(),
-                author.getPaddingBottom());
-        if (IntentUtil.hasAll(intent,
-                IntentUtil.TEXT_COLOR, IntentUtil.FONT_SIZE, IntentUtil.PADDING)) {
-            author.setTextColor(intent.getIntExtra(IntentUtil.TEXT_COLOR, Color.BLACK));
-            float textSize = intent.getFloatExtra(IntentUtil.FONT_SIZE, targetTextSize);
-            author.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
-            Rect padding = intent.getParcelableExtra(IntentUtil.PADDING);
-            author.setPadding(padding.left, padding.top, padding.right, padding.bottom);
+    private var targetTextSize = 0f
+    private var targetTextColors: ColorStateList? = null
+    private var currentDetailBinding: DetailViewBinding? = null
+    private var currentPhotoBinding: PhotoItemBinding? = null
+    private var targetPadding: Rect? = null
+    override fun onSharedElementStart(
+        sharedElementNames: List<String>,
+        sharedElements: List<View>,
+        sharedElementSnapshots: List<View>
+    ) {
+        val author = author
+        targetTextSize = author.textSize
+        targetTextColors = author.textColors
+        targetPadding = Rect(author.paddingLeft,
+            author.paddingTop,
+            author.paddingRight,
+            author.paddingBottom)
+        if (hasAll(
+                intent,
+                IntentUtil.TEXT_COLOR,
+                IntentUtil.FONT_SIZE,
+                IntentUtil.PADDING
+            )
+        ) {
+            author.setTextColor(intent.getIntExtra(IntentUtil.TEXT_COLOR, Color.BLACK))
+            val textSize = intent.getFloatExtra(IntentUtil.FONT_SIZE, targetTextSize)
+            author.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize)
+            val padding = intent.getParcelableExtra<Rect>(IntentUtil.PADDING)
+            author.setPadding(padding!!.left, padding.top, padding.right, padding.bottom)
         }
     }
 
-    @Override
-    public void onSharedElementEnd(List<String> sharedElementNames,
-                                   List<View> sharedElements,
-                                   List<View> sharedElementSnapshots) {
-        TextView author = getAuthor();
-        author.setTextSize(TypedValue.COMPLEX_UNIT_PX, targetTextSize);
+    override fun onSharedElementEnd(
+        sharedElementNames: List<String>,
+        sharedElements: List<View>,
+        sharedElementSnapshots: List<View>
+    ) {
+        val author = author
+        author.setTextSize(TypedValue.COMPLEX_UNIT_PX, targetTextSize)
         if (targetTextColors != null) {
-            author.setTextColor(targetTextColors);
+            author.setTextColor(targetTextColors)
         }
         if (targetPadding != null) {
-            author.setPadding(targetPadding.left, targetPadding.top,
-                    targetPadding.right, targetPadding.bottom);
+            author.setPadding(
+                targetPadding!!.left,
+                targetPadding!!.top,
+                targetPadding!!.right,
+                targetPadding!!.bottom
+            )
         }
         if (currentDetailBinding != null) {
-            forceSharedElementLayout(currentDetailBinding.description);
+            forceSharedElementLayout(currentDetailBinding!!.description)
         }
     }
 
-    @Override
-    public void onMapSharedElements(List<String> names, Map<String, View> sharedElements) {
-        removeObsoleteElements(names, sharedElements, mapObsoleteElements(names));
-        mapSharedElement(names, sharedElements, getAuthor());
-        mapSharedElement(names, sharedElements, getPhoto());
+    override fun onMapSharedElements(
+        names: MutableList<String>,
+        sharedElements: MutableMap<String, View>
+    ) {
+        removeObsoleteElements(names, sharedElements, mapObsoleteElements(names))
+        mapSharedElement(names, sharedElements, author)
+        mapSharedElement(names, sharedElements, photo)
     }
 
-    public void setBinding(@NonNull DetailViewBinding binding) {
-        currentDetailBinding = binding;
-        currentPhotoBinding = null;
+    fun setBinding(binding: DetailViewBinding) {
+        currentDetailBinding = binding
+        currentPhotoBinding = null
     }
 
-    public void setBinding(@NonNull PhotoItemBinding binding) {
-        currentPhotoBinding = binding;
-        currentDetailBinding = null;
+    fun setBinding(binding: PhotoItemBinding) {
+        currentPhotoBinding = binding
+        currentDetailBinding = null
     }
 
-    private TextView getAuthor() {
-        if (currentPhotoBinding != null) {
-            return currentPhotoBinding.author;
-        } else if (currentDetailBinding != null) {
-            return currentDetailBinding.author;
-        } else {
-            throw new NullPointerException("Must set a binding before transitioning.");
+    private val author: TextView
+        get() = when {
+            currentPhotoBinding != null -> {
+                currentPhotoBinding!!.author
+            }
+            currentDetailBinding != null -> {
+                currentDetailBinding!!.author
+            }
+            else -> {
+                throw NullPointerException("Must set a binding before transitioning.")
+            }
         }
-    }
-
-    private ImageView getPhoto() {
-        if (currentPhotoBinding != null) {
-            return currentPhotoBinding.photo;
-        } else if (currentDetailBinding != null) {
-            return currentDetailBinding.photo;
-        } else {
-            throw new NullPointerException("Must set a binding before transitioning.");
+    private val photo: ImageView
+        get() = when {
+            currentPhotoBinding != null -> {
+                currentPhotoBinding!!.photo
+            }
+            currentDetailBinding != null -> {
+                currentDetailBinding!!.photo
+            }
+            else -> {
+                throw NullPointerException("Must set a binding before transitioning.")
+            }
         }
-    }
 
     /**
      * Maps all views that don't start with "android" namespace.
@@ -130,14 +139,13 @@ public class DetailSharedElementEnterCallback extends SharedElementCallback {
      * @param names All shared element names.
      * @return The obsolete shared element names.
      */
-    @NonNull
-    private List<String> mapObsoleteElements(List<String> names) {
-        List<String> elementsToRemove = new ArrayList<>(names.size());
-        for (String name : names) {
-            if (name.startsWith("android")) continue;
-            elementsToRemove.add(name);
+    private fun mapObsoleteElements(names: List<String>): List<String> {
+        val elementsToRemove: MutableList<String> = ArrayList(names.size)
+        for (name in names) {
+            if (name.startsWith("android")) continue
+            elementsToRemove.add(name)
         }
-        return elementsToRemove;
+        return elementsToRemove
     }
 
     /**
@@ -147,13 +155,15 @@ public class DetailSharedElementEnterCallback extends SharedElementCallback {
      * @param sharedElements Shared elements.
      * @param elementsToRemove The elements that should be removed.
      */
-    private void removeObsoleteElements(List<String> names,
-                                        Map<String, View> sharedElements,
-                                        List<String> elementsToRemove) {
-        if (elementsToRemove.size() > 0) {
-            names.removeAll(elementsToRemove);
-            for (String elementToRemove : elementsToRemove) {
-                sharedElements.remove(elementToRemove);
+    private fun removeObsoleteElements(
+        names: MutableList<String>,
+        sharedElements: MutableMap<String, View>,
+        elementsToRemove: List<String>
+    ) {
+        if (elementsToRemove.isNotEmpty()) {
+            names.removeAll(elementsToRemove)
+            for (elementToRemove in elementsToRemove) {
+                sharedElements.remove(elementToRemove)
             }
         }
     }
@@ -165,19 +175,20 @@ public class DetailSharedElementEnterCallback extends SharedElementCallback {
      * @param sharedElements The elements for this transition.
      * @param view The view to add.
      */
-    private void mapSharedElement(List<String> names, Map<String, View> sharedElements, View view) {
-        String transitionName = view.getTransitionName();
-        names.add(transitionName);
-        sharedElements.put(transitionName, view);
+    private fun mapSharedElement(
+        names: MutableList<String>,
+        sharedElements: MutableMap<String, View>,
+        view: View
+    ) {
+        val transitionName = view.transitionName
+        names.add(transitionName)
+        sharedElements[transitionName] = view
     }
 
-    private void forceSharedElementLayout(View view) {
-        int widthSpec = View.MeasureSpec.makeMeasureSpec(view.getWidth(),
-                View.MeasureSpec.EXACTLY);
-        int heightSpec = View.MeasureSpec.makeMeasureSpec(view.getHeight(),
-                View.MeasureSpec.EXACTLY);
-        view.measure(widthSpec, heightSpec);
-        view.layout(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
+    private fun forceSharedElementLayout(view: View) {
+        val widthSpec = View.MeasureSpec.makeMeasureSpec(view.width, View.MeasureSpec.EXACTLY)
+        val heightSpec = View.MeasureSpec.makeMeasureSpec(view.height, View.MeasureSpec.EXACTLY)
+        view.measure(widthSpec, heightSpec)
+        view.layout(view.left, view.top, view.right, view.bottom)
     }
-
 }
