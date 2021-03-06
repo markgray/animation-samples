33a43a2a6b73aabd0187eb285a9f7e3dbe0b997e
markgray <mark.t.gray@gmail.com>
2021-03-06 05:50:05 -0500

    Added comments to UnsplashService in Unsplash

--- Unsplash/app/src/main/java/com/example/android/unsplash/data/UnsplashService.kt
+++ Unsplash/app/src/main/java/com/example/android/unsplash/data/UnsplashService.kt
@@ -15,18 +15,39 @@
  */
 package com.example.android.unsplash.data
 
+import com.example.android.unsplash.MainActivity
 import com.example.android.unsplash.data.model.Photo
 import retrofit.Callback
+import retrofit.RestAdapter
+import retrofit.RetrofitError
 import retrofit.http.GET
 
 /**
  * Modeling the unsplash.it API.
  */
 interface UnsplashService {
+    /**
+     * The `@GET` annotation will make a "GET" request to the REST path "/list" relative to the base
+     * URL. The base URL is set to [ENDPOINT] by a call to the `setEndpoint` method of the
+     * [RestAdapter.Builder] used in the `displayData` method of [MainActivity] so the `GET` call
+     * will download the JSON file it finds at "https://unsplash.it/list", parse it into a [List]
+     * of [Photo] objects which it will feed to the [Callback] parameter [callback] of the [getFeed]
+     * method.
+     *
+     * @param callback the [Callback] which this method will use to return the results of the `GET`
+     * request, either a [List] of [Photo] objects parsed from the JSON at "https://unsplash.it/list"
+     * to the [Callback.success] override of [callback], or the [RetrofitError] if one occurs to the
+     * [Callback.failure] override of [callback].
+     */
     @GET("/list")
     fun getFeed(callback: Callback<List<Photo?>>)
 
     companion object {
+        /**
+         * The base URL for the data this app downloads, used by a call to the `setEndpoint` method
+         * of the [RestAdapter.Builder] used in the `displayData` method of [MainActivity] to create
+         * the instance of [UnsplashService] it uses to download its dataset of [Photo] objects.
+         */
         const val ENDPOINT = "https://unsplash.it"
     }
 }
