ca80dfd0fc2ec23ddadd78a2e6f1bd5afcf0f816
markgray <mark.t.gray@gmail.com>
2021-03-11 07:10:15 -0500

    Converted OnItemSelectedListener to kotlin in Unsplash

--- Unsplash/app/src/main/java/com/example/android/unsplash/ui/grid/OnItemSelectedListener.kt
+++ Unsplash/app/src/main/java/com/example/android/unsplash/ui/grid/OnItemSelectedListener.kt
@@ -13,51 +13,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.unsplash.ui.grid
 
-package com.example.android.unsplash.ui.grid;
+import android.content.Context
+import android.view.GestureDetector
+import android.view.GestureDetector.SimpleOnGestureListener
+import android.view.MotionEvent
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.OnItemTouchListener
 
-import android.content.Context;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
+abstract class OnItemSelectedListener(context: Context?) : OnItemTouchListener {
+    private val gestureDetector: GestureDetector = GestureDetector(context,
+        object : SimpleOnGestureListener() {
+            override fun onSingleTapUp(e: MotionEvent): Boolean {
+                return true
+            }
+        })
 
-import androidx.recyclerview.widget.RecyclerView;
-
-public abstract class OnItemSelectedListener implements RecyclerView.OnItemTouchListener {
-
-    private final GestureDetector gestureDetector;
-
-    public OnItemSelectedListener(Context context) {
-        gestureDetector = new GestureDetector(context,
-                new GestureDetector.SimpleOnGestureListener() {
-                    @Override
-                    public boolean onSingleTapUp(MotionEvent e) {
-                        return true;
-                    }
-                });
-    }
-
-    public abstract void onItemSelected(RecyclerView.ViewHolder holder, int position);
-
-    @Override
-    public final boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+    abstract fun onItemSelected(holder: RecyclerView.ViewHolder, position: Int)
+    override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
         if (gestureDetector.onTouchEvent(e)) {
-            View touchedView = rv.findChildViewUnder(e.getX(), e.getY());
-            onItemSelected(rv.findContainingViewHolder(touchedView),
-                    rv.getChildAdapterPosition(touchedView));
+            val touchedView = rv.findChildViewUnder(e.x, e.y)
+            onItemSelected(rv.findContainingViewHolder(touchedView!!)!!,
+                rv.getChildAdapterPosition(touchedView))
         }
-        return false;
+        return false
     }
 
-
-
-    @Override
-    public final void onTouchEvent(RecyclerView rv, MotionEvent e) {
-        throw new UnsupportedOperationException("Not implemented");
+    override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+        throw UnsupportedOperationException("Not implemented")
     }
 
-    @Override
-    public final void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-        throw new UnsupportedOperationException("Not implemented");
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+        throw UnsupportedOperationException("Not implemented")
     }
+
 }
