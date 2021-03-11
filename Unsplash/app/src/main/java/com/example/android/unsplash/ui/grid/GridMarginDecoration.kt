5bacbbaf0c76d912f0580c8fc28a3fafcf2c24e1
markgray <mark.t.gray@gmail.com>
2021-03-11 06:59:59 -0500

    Added comments to GridMarginDecoration in Unsplash

--- Unsplash/app/src/main/java/com/example/android/unsplash/ui/grid/GridMarginDecoration.kt
+++ Unsplash/app/src/main/java/com/example/android/unsplash/ui/grid/GridMarginDecoration.kt
@@ -19,10 +19,30 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+import com.example.android.unsplash.MainActivity
+import com.example.android.unsplash.R
 
+/**
+ * The `setupRecyclerView` method of [MainActivity] adds an instance of this to its [RecyclerView]
+ * constructed to use a [space] of [R.dimen.grid_item_spacing] (2dp) as an [RecyclerView.ItemDecoration]
+ *
+ * @param space
+ */
 class GridMarginDecoration(
     private val space: Int
     ) : ItemDecoration() {
+    /**
+     * Retrieve any offsets for the given item. Each field of [outRect] specifies the number of
+     * pixels that the item view should be inset by, similar to padding or margin. The default
+     * implementation sets the bounds of outRect to 0 and returns. We set all four fields of our
+     * [Rect] parameter [outRect] (`left`, `top`, `right`, and `bottom`) to the value of [space]
+     * with which we were constructed with.
+     *
+     * @param outRect [Rect] to receive the output.
+     * @param view    The child view to decorate
+     * @param parent  [RecyclerView] this `ItemDecoration` is decorating
+     * @param state   The current state of [RecyclerView].
+     */
     override fun getItemOffsets(
         outRect: Rect,
         view: View,
