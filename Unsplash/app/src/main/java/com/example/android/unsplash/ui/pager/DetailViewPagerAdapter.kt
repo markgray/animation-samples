4a38e5e36ac588bc5cfafe5d1986054957897887
markgray <mark.t.gray@gmail.com>
2021-03-14 07:59:24 -0400

    Converted DetailViewPagerAdapter to kotlin in Unsplash

--- Unsplash/app/src/main/java/com/example/android/unsplash/ui/pager/DetailViewPagerAdapter.kt
+++ Unsplash/app/src/main/java/com/example/android/unsplash/ui/pager/DetailViewPagerAdapter.kt
@@ -13,86 +13,74 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.unsplash.ui.pager;
+package com.example.android.unsplash.ui.pager
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-import androidx.viewpager.widget.PagerAdapter;
-
-import com.bumptech.glide.Glide;
-import com.example.android.unsplash.R;
-import com.example.android.unsplash.data.model.Photo;
-import com.example.android.unsplash.databinding.DetailViewBinding;
-import com.example.android.unsplash.ui.DetailSharedElementEnterCallback;
-import com.example.android.unsplash.ui.ImageSize;
-
-import java.util.List;
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.viewpager.widget.PagerAdapter
+import com.bumptech.glide.Glide
+import com.example.android.unsplash.R
+import com.example.android.unsplash.data.model.Photo
+import com.example.android.unsplash.databinding.DetailViewBinding
+import com.example.android.unsplash.ui.DetailSharedElementEnterCallback
+import com.example.android.unsplash.ui.ImageSize
 
 /**
  * Adapter for paging detail views.
  */
+class DetailViewPagerAdapter(
+    activity: AppCompatActivity,
+    photos: List<Photo>,
+    callback: DetailSharedElementEnterCallback
+) : PagerAdapter() {
+    private val allPhotos: List<Photo> = photos
+    private val layoutInflater: LayoutInflater = LayoutInflater.from(activity)
+    private val photoWidth: Int = activity.resources.displayMetrics.widthPixels
+    private val host: AppCompatActivity = activity
+    private val sharedElementCallback: DetailSharedElementEnterCallback = callback
 
-public class DetailViewPagerAdapter extends PagerAdapter {
-
-    private final List<Photo> allPhotos;
-    private final LayoutInflater layoutInflater;
-    private final int photoWidth;
-    private final AppCompatActivity host;
-    private DetailSharedElementEnterCallback sharedElementCallback;
-
-    public DetailViewPagerAdapter(@NonNull AppCompatActivity activity, @NonNull List<Photo> photos,
-                                  @NonNull DetailSharedElementEnterCallback callback) {
-        layoutInflater = LayoutInflater.from(activity);
-        allPhotos = photos;
-        photoWidth = activity.getResources().getDisplayMetrics().widthPixels;
-        host = activity;
-        sharedElementCallback = callback;
+    override fun getCount(): Int {
+        return allPhotos.size
     }
 
-    @Override
-    public int getCount() {
-        return allPhotos.size();
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val binding: DetailViewBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.detail_view,
+            container,
+            false
+        )
+        binding.data = allPhotos[position]
+        onViewBound(binding)
+        binding.executePendingBindings()
+        container.addView(binding.root)
+        return binding
     }
 
-    @Override
-    public Object instantiateItem(@NonNull ViewGroup container, int position) {
-        DetailViewBinding binding =
-                DataBindingUtil.inflate(layoutInflater, R.layout.detail_view, container, false);
-        binding.setData(allPhotos.get(position));
-        onViewBound(binding);
-        binding.executePendingBindings();
-        container.addView(binding.getRoot());
-        return binding;
-    }
-
-    private void onViewBound(DetailViewBinding binding) {
+    private fun onViewBound(binding: DetailViewBinding) {
         Glide.with(host)
-                .load(binding.getData().getPhotoUrl(photoWidth))
-                .placeholder(R.color.placeholder)
-                .override(ImageSize.NORMAL[0], ImageSize.NORMAL[1])
-                .into(binding.photo);
+            .load(binding.data!!.getPhotoUrl(photoWidth))
+            .placeholder(R.color.placeholder)
+            .override(ImageSize.NORMAL[0], ImageSize.NORMAL[1])
+            .into(binding.photo)
     }
 
-    @Override
-    public void setPrimaryItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-        if (object instanceof DetailViewBinding) {
-            sharedElementCallback.setBinding((DetailViewBinding) object);
+    override fun setPrimaryItem(container: ViewGroup, position: Int, binding: Any) {
+        if (binding is DetailViewBinding) {
+            sharedElementCallback.setBinding(binding)
         }
     }
 
-    @Override
-    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return object instanceof DetailViewBinding
-                && view.equals(((DetailViewBinding) object).getRoot());
+    override fun isViewFromObject(view: View, binding: Any): Boolean {
+        return (binding is DetailViewBinding
+            && view == binding.root)
     }
 
-    @Override
-    public void destroyItem(ViewGroup container, int position, @NonNull Object object) {
-        container.removeView(((DetailViewBinding) object).getRoot());
+    override fun destroyItem(container: ViewGroup, position: Int, binding: Any) {
+        container.removeView((binding as DetailViewBinding).root)
     }
+
 }
