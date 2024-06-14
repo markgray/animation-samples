f3e579e2ec5da179abb23e24075646a71c090401
markgray <mark.t.gray@gmail.com>
2024-06-14 06:16:33 -0400

    Converted build files to kts in GridToPager

--- GridToPager/settings.gradle.kts
+++ GridToPager/settings.gradle.kts
@@ -13,31 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-buildscript {
-    ext.kotlin_version = "1.9.24"
+pluginManagement {
     repositories {
         google()
         mavenCentral()
-    }
-    dependencies {
-        classpath ("com.android.tools.build:gradle:8.4.1")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        gradlePluginPortal()
     }
 }
-
-allprojects {
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage") // TODO: Keep an eye on this
     repositories {
         google()
         mavenCentral()
     }
 }
-
-tasks.register("clean", Delete) {
-    delete (rootProject.layout.buildDir)
-}
+include(":app")
