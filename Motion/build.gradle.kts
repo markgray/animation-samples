d507129c0a0a67b22628a0a270c4ae96b9c23be3
markgray <mark.t.gray@gmail.com>
2024-06-14 07:22:00 -0400

    converted build files to kts, updated AGP to 8.5.0, and updated gradle dependencies in Motion

--- Motion/build.gradle.kts
+++ Motion/build.gradle.kts
@@ -14,26 +14,10 @@
  * limitations under the License.
  */
 
-buildscript {
-    ext.kotlin_version = "1.9.24"
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath ("com.android.tools.build:gradle:8.4.1")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-        (classpath "androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
-    }
-}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.register("clean", Delete) {
-    delete (rootProject.layout.buildDir)
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("androidx.navigation.safeargs") version "2.7.7" apply false
 }
