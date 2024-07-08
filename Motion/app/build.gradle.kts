741b6cdba581d5e1b8567841d01331b0be70c7b0
markgray <mark.t.gray@gmail.com>
2024-07-08 06:34:47 -0400

    Updated kotlin to 2.0.0, and updated gradle dependencies in Motion

--- Motion/app/build.gradle.kts
+++ Motion/app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.fragment:fragment-ktx:1.8.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.transition:transition:1.5.0")
     implementation("androidx.dynamicanimation:dynamicanimation:1.1.0-alpha03")
@@ -63,15 +63,15 @@ dependencies {
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.3")
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("com.google.truth:truth:1.4.2")
-    testImplementation("androidx.test:core:1.5.0")
-    androidTestImplementation("androidx.test.ext:truth:1.5.0")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation("androidx.test:core:1.6.1")
+    androidTestImplementation("androidx.test.ext:truth:1.6.0")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
--- Motion/build.gradle.kts
+++ Motion/build.gradle.kts
@@ -18,6 +18,6 @@
 
 plugins {
     id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("androidx.navigation.safeargs") version "2.7.7" apply false
 }
