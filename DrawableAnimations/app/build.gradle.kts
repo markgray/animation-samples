e1f145c112e6958049a6bed0054715408d2e2027
markgray <mark.t.gray@gmail.com>
2024-07-08 05:59:46 -0400

    Updated kotlin to 2.0.0, and updated gradle dependencies in DrawableAnimations

--- DrawableAnimations/app/build.gradle.kts
+++ DrawableAnimations/app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
 
 dependencies {
     implementation("androidx.activity:activity-ktx:1.9.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -64,8 +64,8 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("com.google.truth:truth:1.4.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    debugImplementation("androidx.fragment:fragment-testing:1.8.0")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    debugImplementation("androidx.fragment:fragment-testing:1.8.1")
     androidTestImplementation("androidx.core:core-animation-testing:1.0.0")
 }
