20b0f23c30c6aa75276345c69f6ee3823cd2e277
markgray <mark.t.gray@gmail.com>
2024-09-12 05:32:52 -0400

    Updated gradle dependencies in Motion

--- Motion/app/build.gradle.kts
+++ Motion/app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.fragment:fragment-ktx:1.8.2")
+    implementation("androidx.fragment:fragment-ktx:1.8.3")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.transition:transition:1.5.1")
     implementation("androidx.dynamicanimation:dynamicanimation:1.1.0-alpha03")
@@ -61,15 +61,15 @@ dependencies {
     implementation("androidx.paging:paging-runtime-ktx:3.3.2")
     implementation("com.google.android.material:material:1.12.0")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.5")
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("com.google.truth:truth:1.4.2")
+    testImplementation("com.google.truth:truth:1.4.4")
     testImplementation("androidx.test:core:1.6.1")
     androidTestImplementation("androidx.test.ext:truth:1.6.0")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
--- Motion/build.gradle.kts
+++ Motion/build.gradle.kts
@@ -19,5 +19,5 @@
 plugins {
     id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("androidx.navigation.safeargs") version "2.7.7" apply false
+    id("androidx.navigation.safeargs") version "2.8.0" apply false
 }
