802a4da1fc4ca68a0d58c6da1d436e8678c4f889
Mark Gray <mark.t.gray@gmail.com>
2024-10-28 04:32:58 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 in DrawableAnimations

--- DrawableAnimations/build.gradle.kts
+++ DrawableAnimations/build.gradle.kts
@@ -16,5 +16,5 @@
 
 plugins {
     id("com.android.application") version "8.7.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
