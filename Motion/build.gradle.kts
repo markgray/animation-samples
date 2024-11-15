c859633c84f0d09dc2bc2451b843e80c9549068d
Mark Gray <mark.t.gray@gmail.com>
2024-11-15 03:36:55 -0500

    Updated gradle dependencies in Motion

--- Motion/build.gradle.kts
+++ Motion/build.gradle.kts
@@ -19,5 +19,5 @@
 plugins {
     id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("androidx.navigation.safeargs") version "2.8.3" apply false
+    id("androidx.navigation.safeargs") version "2.8.4" apply false
 }
