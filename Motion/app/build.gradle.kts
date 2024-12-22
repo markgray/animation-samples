51cda29220336aefacb18f878c6149da962cd43c
Mark Gray <mark.t.gray@gmail.com>
2024-12-22 04:00:21 -0500

    Updated gradle dependencies in Motion

--- Motion/app/build.gradle.kts
+++ Motion/app/build.gradle.kts
@@ -59,11 +59,11 @@ dependencies {
     implementation("androidx.dynamicanimation:dynamicanimation:1.1.0-alpha03")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-    implementation("androidx.paging:paging-runtime-ktx:3.3.4")
+    implementation("androidx.paging:paging-runtime-ktx:3.3.5")
     implementation("com.google.android.material:material:1.12.0")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.5")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
 
