06cc9e8b78e3a874e90a6fdd3603fa6f11861f86
markgray <mark.t.gray@gmail.com>
2025-03-09 08:26:57 -0400

    Updated AGP to 8.9.0 and updated gradle dependencies in GridToPager

--- GridToPager/app/build.gradle.kts
+++ GridToPager/app/build.gradle.kts
@@ -50,9 +50,9 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.viewpager2:viewpager2:1.1.0")
