a026eb3fa8fd82442a848b944a51a7f4e71cda1d
Mark Gray <mark.t.gray@gmail.com>
2025-04-15 03:22:11 -0400

    Updated gradle dependency in GridToPager

--- GridToPager/app/build.gradle.kts
+++ GridToPager/app/build.gradle.kts
@@ -61,5 +61,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.test:rules:1.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
 }
