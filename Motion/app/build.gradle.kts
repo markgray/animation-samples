35d76fd6d85ee6f790a33f4e401a445514b44381
markgray <mark.t.gray@gmail.com>
2025-05-15 05:47:41 -0400

    Updated AGP to 8.10.0, updated gradle to 8.14, updated kotlin to 2.1.21 and updated gradle dependencies in Motion

--- Motion/app/build.gradle.kts
+++ Motion/app/build.gradle.kts
@@ -62,10 +62,10 @@ dependencies {
     implementation("androidx.paging:paging-runtime-ktx:3.3.6")
     implementation("com.google.android.material:material:1.12.0")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.9")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.9")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.0")
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
