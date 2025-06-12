9482c21c81838e2a736e4ad4d2be4ed091d4d8d3
Mark Gray <mark.t.gray@gmail.com>
2025-06-12 04:10:39 -0400

    Updated AGP to 8.10.1, and updated gradle to 8.14.2 and updated gradle dependencies in Motion

--- Motion/app/build.gradle.kts
+++ Motion/app/build.gradle.kts
@@ -53,8 +53,8 @@ android {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.7")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.8")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.transition:transition:1.6.0")
     implementation("androidx.dynamicanimation:dynamicanimation-ktx:1.1.0")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
@@ -64,8 +64,8 @@ dependencies {
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.1")
 
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
