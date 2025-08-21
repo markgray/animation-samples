62f032e60fb98ecad111994308ff3576c020c84b
markgray <mark.t.gray@gmail.com>
2025-08-21 07:03:38 -0400

    Updated AGP to 8.12.1, updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in Unsplash

--- Unsplash/app/build.gradle.kts
+++ Unsplash/app/build.gradle.kts
@@ -39,10 +39,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.8")
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
     implementation("androidx.annotation:annotation:1.9.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.squareup.retrofit:retrofit:1.9.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
 }
