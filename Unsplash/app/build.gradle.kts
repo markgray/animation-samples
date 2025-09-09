e02449182f03d82e5aa714715292d8bc4f9e046a
markgray <mark.t.gray@gmail.com>
2025-09-09 08:20:53 -0400

    Updated AGP to 8.13.0, fixed warnings, and updated gradle dependencies in Unsplash

--- Unsplash/app/build.gradle.kts
+++ Unsplash/app/build.gradle.kts
@@ -38,11 +38,11 @@ android {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.fragment:fragment-ktx:1.8.9")
     implementation("androidx.annotation:annotation:1.9.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.squareup.retrofit:retrofit:1.9.0")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.github.bumptech.glide:glide:5.0.4")
     implementation("androidx.core:core-ktx:1.17.0")
 }
