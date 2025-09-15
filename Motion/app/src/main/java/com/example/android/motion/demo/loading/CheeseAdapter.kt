58137cfb756bb112f3e07d92abc9dc86ac6df889
markgray <mark.t.gray@gmail.com>
2025-09-15 03:28:14 -0400

    Added comments in Motion

--- Motion/app/src/main/java/com/example/android/motion/demo/loading/CheeseAdapter.kt
+++ Motion/app/src/main/java/com/example/android/motion/demo/loading/CheeseAdapter.kt
@@ -36,13 +36,15 @@ import com.example.android.motion.model.Cheese
 /**
  * The adapter that displays the list of [Cheese] objects using Paging 3.
  */
-internal class CheeseAdapter : PagingDataAdapter<Cheese, CheeseViewHolder>(Cheese.DIFF_CALLBACK) {
+internal class CheeseAdapter : PagingDataAdapter<Cheese, CheeseViewHolder>(
+    Cheese.DIFF_CALLBACK
+) {
 
     /**
      * Called when RecyclerView needs a new [CheeseViewHolder] of the given type to represent
      * an item. This new ViewHolder should be constructed with a new View that can represent the
      * items of the given type. You can either create a new View manually or inflate it from an XML
-     * layout file.
+     * layout file. We return a new instance of [CheeseViewHolder]
      *
      * @param parent The ViewGroup into which the new View will be added after it is bound to
      * an adapter position.
@@ -58,9 +60,11 @@ internal class CheeseAdapter : PagingDataAdapter<Cheese, CheeseViewHolder>(Chees
      * updates the contents of the [CheeseViewHolder.itemView] to reflect the item at the
      * given position.
      *
-     * It retrieves the [Cheese] item for the given `position`. If the item is not null, it
-     * binds the data to the `holder`. If the item is null (which can happen with placeholders
-     * in `PagingDataAdapter`, though disabled in our configuration), it shows a placeholder UI.
+     * It retrieves the [Cheese] item for the given `position` to initialize its [Cheese] variable
+     * `cheese`. If `cheese` is not null, it binds the data to [CheeseViewHolder] parameter [holder].
+     * If the item is `null` (which can happen with placeholders in [PagingDataAdapter], though
+     * disabled in our configuration), it calls the [CheeseViewHolder.showPlaceholder] method of
+     * [holder] to show a placeholder UI.
      *
      * @param holder The [CheeseViewHolder] which should be updated to represent the contents of the
      * item at the given `position` in the data set.
@@ -68,7 +72,7 @@ internal class CheeseAdapter : PagingDataAdapter<Cheese, CheeseViewHolder>(Chees
      */
     override fun onBindViewHolder(holder: CheeseViewHolder, position: Int) {
         val cheese: Cheese? = getItem(position)
-        // Note: PagingDataAdapter may present null items if placeholders are enabled
+        // Note: PagingDataAdapter may present `null` items if placeholders are enabled
         // and data is not yet loaded. Our PagingConfig has placeholders disabled.
         if (cheese != null) {
             holder.bind(cheese)
@@ -76,6 +80,7 @@ internal class CheeseAdapter : PagingDataAdapter<Cheese, CheeseViewHolder>(Chees
             // Optionally, handle the case where item is null, though with placeholders disabled
             // this path should ideally not be hit for actual list items.
             // If you still want placeholder UI for loading states, consider using LoadStateAdapter.
+            // TODO: Switch to LoadStateAdapter
             holder.showPlaceholder() // Kept for consistency if needed, but review usage.
         }
     }
@@ -89,13 +94,12 @@ internal class CheeseAdapter : PagingDataAdapter<Cheese, CheeseViewHolder>(Chees
 private const val FADE_DURATION = 1000L
 
 /**
- * This is the view holder used for the [CheeseAdapter]. It
- * can "bind" to display a [Cheese] object, or just be used to display a flashing empty placeholder
- * view while "waiting" for the [Cheese] objects to be "downloaded".
- * The constructor uses the [LayoutInflater] from the context of our [ViewGroup]
- * parameter `parent` to inflate our layout file `R.layout.cheese_list_item` to be used as the
- * item view for our [RecyclerView.ViewHolder]. This layout file consists of a `LinearLayout` holding
- * an [ImageView] with ID `R.id.image` and a [TextView] with ID `R.id.name`.
+ * This is the view holder used for the [CheeseAdapter]. It can "bind" to display a [Cheese] object,
+ * or just be used to display a flashing empty placeholder view while "waiting" for the [Cheese]
+ * objects to be "downloaded". The constructor uses the [LayoutInflater] from the context of our
+ * [ViewGroup] parameter `parent` to inflate our layout file `R.layout.cheese_list_item` to be used
+ * as the [itemView] for our [RecyclerView.ViewHolder]. This layout file consists of a `LinearLayout`
+ * holding an [ImageView] with ID `R.id.image` and a [TextView] with ID `R.id.name`.
  *
  * We initialize our [ImageView] property [image] to the [ImageView] with ID `R.id.image` in the
  * [itemView] we inflated from the layout file with ID `R.layout.cheese_list_item`. We initialize
