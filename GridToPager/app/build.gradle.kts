e73100ee470352bcba07e49b1e2525fc0dcb25e1
markgray <mark.t.gray@gmail.com>
2025-09-23 08:29:41 -0400

    Updated kotlin to 2.2.20, fixed warnings, and updated gradle dependencies in GridToPager

--- GridToPager/app/build.gradle.kts
+++ GridToPager/app/build.gradle.kts
@@ -38,14 +38,14 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.activity:activity-ktx:1.11.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation("androidx.annotation:annotation:1.9.1")
-    implementation("com.github.bumptech.glide:glide:5.0.4")
+    implementation("com.github.bumptech.glide:glide:5.0.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
     androidTestImplementation("androidx.test:rules:1.7.0")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
