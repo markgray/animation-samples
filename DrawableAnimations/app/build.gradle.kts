420b978af2317349953101b1464375a4c33dc017
markgray <mark.t.gray@gmail.com>
2025-09-23 08:24:52 -0400

    Updated kotlin to 2.2.20, fixed warnings, and updated gradle dependencies in DrawableAnimations

--- DrawableAnimations/app/build.gradle.kts
+++ DrawableAnimations/app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.activity:activity-ktx:1.11.0")
     implementation("androidx.fragment:fragment-ktx:1.8.9")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.core:core-ktx:1.17.0")
@@ -51,7 +51,7 @@ dependencies {
     implementation("androidx.vectordrawable:vectordrawable-seekable:1.0.0")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("com.google.truth:truth:1.4.4")
+    androidTestImplementation("com.google.truth:truth:1.4.5")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
     debugImplementation("androidx.fragment:fragment-testing:1.8.9")
