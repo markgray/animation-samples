14bd2b5c3f88c70cad59ee3fded1faf72a9074df
markgray <mark.t.gray@gmail.com>
2025-12-11 04:27:13 -0500

    Added ime padding in DrawableAnimations

--- DrawableAnimations/app/src/main/java/com/example/android/drawableanimations/MainActivity.kt
+++ DrawableAnimations/app/src/main/java/com/example/android/drawableanimations/MainActivity.kt
@@ -53,13 +53,15 @@ class MainActivity : AppCompatActivity(R.layout.main_activity) {
      * method to set an [OnApplyWindowInsetsListener] to take over the policy for applying window
      * insets to `rootView`, with the `listener` argument a lambda that accepts the [View] passed
      * the lambda in variable `v` and the [WindowInsetsCompat] passed the lambda in variable
-     * `windowInsets`. It initializes its [Insets] variable `insets` to the
+     * `windowInsets`. It initializes its [Insets] variable `systemBars` to the
      * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
-     * as the argument, then it updates the layout parameters of `v` to be a
-     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`, the right margin set
-     * to `insets.right`, the top margin set to `insets.top`, and the bottom margin set to
-     * `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so that the
-     * window insets will not keep passing down to descendant views).
+     * as the argument. It then gets the insets for the IME (keyboard) using
+     * [WindowInsetsCompat.Type.ime]. It then updates the layout parameters of `v` to be a
+     * [ViewGroup.MarginLayoutParams] with the left margin set to `systemBars.left`, the right
+     * margin set to `systemBars.right`, the top margin set to `systemBars.top`, and the bottom
+     * margin set to the maximum of the system bars bottom inset and the IME bottom inset.
+     * Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so that the window insets
+     * will not keep passing down to descendant views).
      *
      * If our [Bundle] parameter [savedInstanceState] is `null` this is the first time we have
      * been called, so we use the `commitNow` method of the `FragmentManager` for interacting with
@@ -76,15 +78,16 @@ class MainActivity : AppCompatActivity(R.layout.main_activity) {
 
         val rootView = findViewById<FragmentContainerView>(R.id.main)
         ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
-            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val ime = windowInsets.getInsets(WindowInsetsCompat.Type.ime())
+
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                leftMargin = insets.left
-                bottomMargin = insets.bottom
-                rightMargin = insets.right
-                topMargin = insets.top
+                leftMargin = systemBars.left
+                rightMargin = systemBars.right
+                topMargin = systemBars.top
+                bottomMargin = systemBars.bottom.coerceAtLeast(ime.bottom)
             }
-
             // Return CONSUMED if you don't want want the window insets to keep passing
             // down to descendant views.
             WindowInsetsCompat.CONSUMED
