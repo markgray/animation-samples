f58244439844909a9e0f5750d784f6a9a8338799
markgray <mark.t.gray@gmail.com>
2025-12-11 04:05:05 -0500

    Added ime padding in ActivitySceneTransitionBasic

--- ActivitySceneTransitionBasic/app/src/main/java/com/example/android/activityscenetransitionbasic/DetailActivity.kt
+++ ActivitySceneTransitionBasic/app/src/main/java/com/example/android/activityscenetransitionbasic/DetailActivity.kt
@@ -17,12 +17,14 @@ package com.example.android.activityscenetransitionbasic
 
 import android.os.Bundle
 import android.transition.Transition
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.ScrollView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -52,19 +54,36 @@ class DetailActivity : AppCompatActivity() {
     private lateinit var mItem: Item
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `(onCreate)`,
-     * then we set our content view to our layout file `R.layout.details`. First we initialize our
-     * [Item] field [mItem] by calling the [Item.getItem] static method to retrieve the [Item] in
-     * the dataset [Item.ITEMS] which has the ID that was passed us as an extra in the `Intent` that
-     * launched us stored under the key [EXTRA_PARAM_ID] ("detail:_id"). We then initialize our
-     * [ImageView] field [mHeaderImageView] by finding the view with ID `R.id.imageview_header`, and
-     * initialize our [TextView] field [mHeaderTitle] by finding the view with ID `R.id.textview_title`.
-     * We then set the name of [mHeaderImageView] to be used to identify Views in Transitions to
-     * [VIEW_NAME_HEADER_IMAGE] ("detail:header:image"), and set the name of [mHeaderTitle] to be
-     * used to identify Views in Transitions to [VIEW_NAME_HEADER_TITLE] ("detail:header:title").
-     * Finally we call our method [loadItem] to have it set the text of [mHeaderTitle] and to call
-     * either [loadThumbnail] or [loadFullSizeImage] as appropriate to load the correct image into
-     * [mHeaderImageView].
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to
+     * edge display, then we call our super's implementation of `onCreate`, and set our content
+     * view to our layout file `R.layout.details`.
+     *
+     * We initialize our [ScrollView] variable `rootView` to the view with ID `R.id.root_view`
+     * then call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy for applying
+     * window insets to `rootView`, with the `listener` argument a lambda that accepts the [View]
+     * passed the lambda in variable `v` and the [WindowInsetsCompat] passed the lambda in variable
+     * `windowInsets`. It initializes its [Insets] variable `systemBars` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
+     * as the argument. It then gets the insets for the IME (keyboard) using
+     * [WindowInsetsCompat.Type.ime]. It then updates the layout parameters of `v` to be a
+     * [ViewGroup.MarginLayoutParams] with the left margin set to `systemBars.left`, the right
+     * margin set to `systemBars.right`, the top margin set to `systemBars.top`, and the bottom
+     * margin set to the maximum of the system bars bottom inset and the IME bottom inset.
+     * Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so that the window insets
+     * will not keep passing down to descendant views).
+     *
+     *
+     * Next we initialize our [Item] field [mItem] by calling the [Item.getItem] static method to
+     * retrieve the [Item] in the dataset [Item.ITEMS] which has the ID that was passed us as an
+     * extra in the `Intent` that launched us stored under the key [EXTRA_PARAM_ID] ("detail:_id").
+     * We then initialize our [ImageView] field [mHeaderImageView] by finding the view with ID
+     * `R.id.imageview_header`, and initialize our [TextView] field [mHeaderTitle] by finding the
+     * view with ID `R.id.textview_title`. We then set the name of [mHeaderImageView] to be used to
+     * identify Views in Transitions to [VIEW_NAME_HEADER_IMAGE] ("detail:header:image"), and set
+     * the name of [mHeaderTitle] to be used to identify Views in Transitions to
+     * [VIEW_NAME_HEADER_TITLE] ("detail:header:title"). Finally we call our method [loadItem] to
+     * have it set the text of [mHeaderTitle] and to call either [loadThumbnail] or
+     * [loadFullSizeImage] as appropriate to load the correct image into [mHeaderImageView].
      *
      * @param savedInstanceState We do not call [onSaveInstanceState] so do not use.
      */
@@ -73,16 +92,17 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.details)
         val rootView = findViewById<ScrollView>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val ime = windowInsets.getInsets(WindowInsetsCompat.Type.ime())
+
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                leftMargin = insets.left
-                bottomMargin = insets.bottom
-                rightMargin = insets.right
-                topMargin = insets.top
+                leftMargin = systemBars.left
+                rightMargin = systemBars.right
+                topMargin = systemBars.top
+                bottomMargin = systemBars.bottom.coerceAtLeast(ime.bottom)
             }
-
             // Return CONSUMED if you don't want want the window insets to keep passing
             // down to descendant views.
             WindowInsetsCompat.CONSUMED
--- ActivitySceneTransitionBasic/app/src/main/java/com/example/android/activityscenetransitionbasic/MainActivity.kt
+++ ActivitySceneTransitionBasic/app/src/main/java/com/example/android/activityscenetransitionbasic/MainActivity.kt
@@ -51,17 +51,17 @@ class MainActivity : AppCompatActivity() {
      * to our [OnItemClickListener] field [mOnItemClickListener]. We initialize our [GridAdapter]
      * variable `val adapter` to a new instance, and set the `adapter` of `grid` to it.
      *
-     * We then call [ViewCompat.setOnApplyWindowInsetsListener] to
-     * take over the policy for applying window insets to `grid`, with the listener
-     * argument a lambda that accepts the [View] passed the lambda in variable `v` and
-     * the [WindowInsetsCompat] passed the lambda in variable `windowInsets`. It
-     * initializes its [Insets] variable `insets` to the [WindowInsetsCompat.getInsets]
-     * of `windowInsets` with [WindowInsetsCompat.Type.systemBars] as the
-     * argument, then it updates the layout parameters of `v` to be a
-     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`,
-     * the right margin set to `insets.right`, the top margin set to `insets.top`,
-     * and the bottom margin set to `insets.bottom`. Finally it returns
-     * [WindowInsetsCompat.CONSUMED] to the caller (so that the window insets
+     * We then call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy for
+     * applying window insets to `grid`, with the listener argument a lambda that accepts the
+     * [View] passed the lambda in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable `systemBars` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
+     * as the argument. It then gets the insets for the IME (keyboard) using
+     * [WindowInsetsCompat.Type.ime]. It then updates the layout parameters of `v` to be a
+     * [ViewGroup.MarginLayoutParams] with the left margin set to `systemBars.left`, the right
+     * margin set to `systemBars.right`, the top margin set to `systemBars.top`, and the bottom
+     * margin set to the maximum of the system bars bottom inset and the IME bottom inset.
+     * Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so that the window insets
      * will not keep passing down to descendant views).
      *
      * @param savedInstanceState We do not call [onSaveInstanceState] so do not use.
@@ -78,15 +78,16 @@ class MainActivity : AppCompatActivity() {
         grid.adapter = adapter
 
         ViewCompat.setOnApplyWindowInsetsListener(grid) { v: View, windowInsets: WindowInsetsCompat ->
-            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val ime = windowInsets.getInsets(WindowInsetsCompat.Type.ime())
+
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                leftMargin = insets.left
-                bottomMargin = insets.bottom
-                rightMargin = insets.right
-                topMargin = insets.top
+                leftMargin = systemBars.left
+                rightMargin = systemBars.right
+                topMargin = systemBars.top
+                bottomMargin = systemBars.bottom.coerceAtLeast(ime.bottom)
             }
-
             // Return CONSUMED if you don't want want the window insets to keep passing
             // down to descendant views.
             WindowInsetsCompat.CONSUMED
